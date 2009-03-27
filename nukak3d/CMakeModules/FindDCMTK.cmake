18edcab68923a8532c366693285ee02077455c27
apinzonf <apinzonf@4b68e429-1748-0410-913f-c2fc311d3372>
2009-03-27 19:46:40 +0000

    DCMTK: not need library's and header's
    git-svn-id: svn://svn.code.sf.net/p/nukak3d/code/trunk@84 4b68e429-1748-0410-913f-c2fc311d3372

--- nukak3d/CMakeModules/FindDCMTK.cmake
+++ nukak3d/CMakeModules/FindDCMTK.cmake
@@ -12,125 +12,59 @@
 # the source.
 #
 # Written for VXL by Amitha Perera.
+# Modified by Alexander Pinzon Fernandez
 # 
 
-FIND_PATH( DCMTK_config_INCLUDE_DIR osconfig.h
-  ${DCMTK_DIR}/config/include 
-  ${DCMTK_DIR}/include
-  ${DCMTK_DIR}/include/dcmtk/config
+SET(DCMTK_PROGRAM_PATHS ${DCMTK_DIR}
+	${DCMTK_DIR}/bin
+	${DCMTK_DIR}/../install/bin
+	${DCMTK_DIR}/../dcmtk-3.5.4-linux-i686
+	${DCMTK_DIR}/../dcmtk-3.5.4-linux-i686/bin
+	${DCMTK_DIR}/../dcmtk-3.5.4-solaris9-sparc
+	${DCMTK_DIR}/../dcmtk-3.5.4-solaris9-sparc/bin
+	${DCMTK_DIR}/../dcmtk-3.5.4-win32-i386
+	${DCMTK_DIR}/../dcmtk-3.5.4-win32-i386/bin 
+	/usr/bin
+	/usr/local/bin
+	/opt/local/bin
 )
 
-FIND_PATH( DCMTK_ofstd_INCLUDE_DIR ofstdinc.h
-  ${DCMTK_DIR}/ofstd/include 
-  ${DCMTK_DIR}/include/ofstd
-  ${DCMTK_DIR}/include/dcmtk/ofstd
+FIND_PROGRAM(DCMTK_dump2dcm_PROGRAM
+	NAMES dump2dcm
+	PATHS ${DCMTK_PROGRAM_PATHS}
 )
-
-FIND_LIBRARY( DCMTK_ofstd_LIBRARY ofstd
-  ${DCMTK_DIR}/ofstd/libsrc
-  ${DCMTK_DIR}/ofstd/libsrc/Release
-  ${DCMTK_DIR}/ofstd/libsrc/Debug
-  ${DCMTK_DIR}/ofstd/Release
-  ${DCMTK_DIR}/ofstd/Debug
-  ${DCMTK_DIR}/lib
+FIND_PROGRAM(DCMTK_echoscu_PROGRAM 
+	NAMES echoscu
+	PATHS ${DCMTK_PROGRAM_PATHS}
 )
-
-
-FIND_PATH( DCMTK_dcmdata_INCLUDE_DIR dctypes.h
-  ${DCMTK_DIR}/dcmdata/include
-  ${DCMTK_DIR}/include/dcmdata
-  ${DCMTK_DIR}/include/dcmtk/dcmdata
+FIND_PROGRAM(DCMTK_findscu_PROGRAM
+	NAMES findscu
+	PATHS ${DCMTK_PROGRAM_PATHS}
 )
-
-FIND_LIBRARY( DCMTK_dcmdata_LIBRARY dcmdata
-  ${DCMTK_DIR}/dcmdata/libsrc
-  ${DCMTK_DIR}/dcmdata/libsrc/Release
-  ${DCMTK_DIR}/dcmdata/libsrc/Debug
-  ${DCMTK_DIR}/dcmdata/Release
-  ${DCMTK_DIR}/dcmdata/Debug
-  ${DCMTK_DIR}/lib
+FIND_PROGRAM(DCMTK_movescu_PROGRAM
+	NAMES movescu
+	PATHS ${DCMTK_PROGRAM_PATHS}
 )
-
-
-FIND_PATH( DCMTK_dcmimgle_INCLUDE_DIR dcmimage.h
-  ${DCMTK_DIR}/dcmimgle/include
-  ${DCMTK_DIR}/include/dcmimgle
-  ${DCMTK_DIR}/include/dcmtk/dcmimgle
-)
-
-FIND_LIBRARY( DCMTK_dcmimgle_LIBRARY dcmimgle
-  ${DCMTK_DIR}/dcmimgle/libsrc
-  ${DCMTK_DIR}/dcmimgle/libsrc/Release
-  ${DCMTK_DIR}/dcmimgle/libsrc/Debug
-  ${DCMTK_DIR}/dcmimgle/Release
-  ${DCMTK_DIR}/dcmimgle/Debug
-  ${DCMTK_DIR}/lib
-)
-
-FIND_LIBRARY(DCMTK_imagedb_LIBRARY imagedb 
-${DCMTK_DIR}/imagectn/libsrc/Release
-${DCMTK_DIR}/imagectn/libsrc/
-${DCMTK_DIR}/imagectn/libsrc/Debug
-)
-
-FIND_LIBRARY(DCMTK_dcmnet_LIBRARY dcmnet 
-${DCMTK_DIR}/dcmnet/libsrc/Release
-${DCMTK_DIR}/dcmnet/libsrc/Debug
-${DCMTK_DIR}/dcmnet/libsrc/
-${DCMTK_DIR}/lib
+FIND_PROGRAM(DCMTK_storescp_PROGRAM
+	NAMES storescp 
+	PATHS ${DCMTK_PROGRAM_PATHS}
 )
 
 
-IF( DCMTK_config_INCLUDE_DIR 
-    AND DCMTK_ofstd_INCLUDE_DIR 
-    AND DCMTK_ofstd_LIBRARY
-    AND DCMTK_dcmdata_INCLUDE_DIR
-    AND DCMTK_dcmdata_LIBRARY
-    AND DCMTK_dcmimgle_INCLUDE_DIR
-    AND DCMTK_dcmimgle_LIBRARY )
+IF( DCMTK_dump2dcm_PROGRAM
+    AND DCMTK_echoscu_PROGRAM 
+    AND DCMTK_findscu_PROGRAM
+    AND DCMTK_movescu_PROGRAM
+    AND DCMTK_storescp_PROGRAM)
 
   SET( DCMTK_FOUND "YES" )
-  SET( DCMTK_INCLUDE_DIR
-    ${DCMTK_config_INCLUDE_DIR}
-    ${DCMTK_ofstd_INCLUDE_DIR}
-    ${DCMTK_dcmdata_INCLUDE_DIR}
-    ${DCMTK_dcmimgle_INCLUDE_DIR}
-  )
-
-  SET( DCMTK_LIBRARIES
-    ${DCMTK_dcmimgle_LIBRARY}
-    ${DCMTK_dcmdata_LIBRARY}
-    ${DCMTK_ofstd_LIBRARY}
-    ${DCMTK_config_LIBRARY}
-  )
-
-  IF(DCMTK_imagedb_LIBRARY)
-   SET( DCMTK_LIBRARIES
-   ${DCMTK_LIBRARIES}
-   ${DCMTK_imagedb_LIBRARY}
-   )
-  ENDIF(DCMTK_imagedb_LIBRARY)
-
-  IF(DCMTK_dcmnet_LIBRARY)
-   SET( DCMTK_LIBRARIES
-   ${DCMTK_LIBRARIES}
-   ${DCMTK_dcmnet_LIBRARY}
-   )
-  ENDIF(DCMTK_dcmnet_LIBRARY)
-
-  IF( WIN32 )
-    SET( DCMTK_LIBRARIES ${DCMTK_LIBRARIES} netapi32 )
-  ENDIF( WIN32 )
-
-ENDIF( DCMTK_config_INCLUDE_DIR 
-    AND DCMTK_ofstd_INCLUDE_DIR 
-    AND DCMTK_ofstd_LIBRARY
-    AND DCMTK_dcmdata_INCLUDE_DIR
-    AND DCMTK_dcmdata_LIBRARY
-    AND DCMTK_dcmimgle_INCLUDE_DIR
-    AND DCMTK_dcmimgle_LIBRARY )
+  
+ENDIF( DCMTK_dump2dcm_PROGRAM
+    AND DCMTK_echoscu_PROGRAM
+    AND DCMTK_findscu_PROGRAM
+    AND DCMTK_movescu_PROGRAM
+    AND DCMTK_storescp_PROGRAM)
 
 IF( NOT DCMTK_FOUND )
   SET( DCMTK_DIR "" CACHE PATH "Root of DCMTK source tree (optional)." )
-  MARK_AS_ADVANCED( DCMTK_DIR )
 ENDIF( NOT DCMTK_FOUND )
